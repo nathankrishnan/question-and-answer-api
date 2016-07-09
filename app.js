685ad830cb1adc0a8b243ee6d9133c0d41143ea5
Nathan <nathank@gwu.edu>
2016-07-09 00:27:50 -0400

    Error handling for 404 and 500 statuses

--- app.js
+++ app.js
@@ -12,6 +12,24 @@ app.use(logger("dev"));
 app.use(jsonParser());
 app.use("/questions", routes);
 
+// Catch 404 and forward to error handler
+app.use(function(req, res, next){
+  var err = new Error("Not Found");
+  err.status = 404;
+  next(err);
+});
+
+// Error handler
+app.use(function(err, req, res, next){
+  // if the error status is undefined, 500 is used
+  res.status(err.status || 500);
+  res.json({
+    error: {
+      message: err.message
+    }
+  });
+
+});
 
 var port = process.env.PORT || 3000;
 
